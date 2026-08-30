7c493c385873cc35bcde9f5d7395ff37355dcde3
NoamMuchtar <noam@nomu.co.il>
2026-08-30 15:14:21 +0300

    fix returnUser

--- users/helpers/returnUser.js
+++ users/helpers/returnUser.js
@@ -1,16 +1,26 @@
 const returnUser = (user) => {
   return {
+    _id: user._id,
+
     name: {
       first: user.name.first,
       middle: user.name.middle,
       last: user.name.last,
     },
+
+    phone: user.phone,
+
     email: user.email,
+
     image: {
       url: user.image.url,
       alt: user.image.alt,
     },
+
+    address: user.address,
+
     isRecruiter: user.isRecruiter,
+
     isAdmin: user.isAdmin,
   };
 };
